d5880dd2db69daddc46dad1950590e3d0ac92174
sajdkabir <sajda.kbir@gmail.com>
2024-09-18 13:08:44 +0530

    hello world

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+
+use axum::{routing::get, Router};
+
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(hello_world));
+    let listener= tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+
+async fn hello_world()-> String{
+    "Hello World!!!".to_owned()
+}
