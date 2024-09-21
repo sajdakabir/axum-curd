a59b224724c687f3b1a8eccaae55e2e0052fca62
sajdkabir <sajda.kbir@gmail.com>
2024-09-21 19:50:17 +0530

    get path variables

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,14 +1,17 @@
 mod hello_world;
 mod mirror_body_string;
 mod mirror_body_json;
+mod path_variables;
 
 use axum::{routing::{get, post}, Router};
 use hello_world::hello_world;
 use mirror_body_string::mirror_body_string;
 use mirror_body_json::mirror_body_json;
+use path_variables::path_variable;
 
 pub fn create_route()-> Router{
     Router::new().route("/hello", get(hello_world))
                 .route("/mirror_body_string", post(mirror_body_string))
                 .route("/mirror_body_json", post(mirror_body_json))
+                .route("/path_variable/:id", get(path_variable))
 }
