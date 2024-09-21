3010da5aae963d79cc327034b4af2ea403858cbd
sajdkabir <sajda.kbir@gmail.com>
2024-09-21 19:53:15 +0530

    orger doesn't matter in axum

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -7,11 +7,12 @@ use axum::{routing::{get, post}, Router};
 use hello_world::hello_world;
 use mirror_body_string::mirror_body_string;
 use mirror_body_json::mirror_body_json;
-use path_variables::path_variable;
+use path_variables::{hard_coded, path_variable};
 
 pub fn create_route()-> Router{
     Router::new().route("/hello", get(hello_world))
                 .route("/mirror_body_string", post(mirror_body_string))
                 .route("/mirror_body_json", post(mirror_body_json))
                 .route("/path_variable/:id", get(path_variable))
+                .route("/path_variable/100", get(hard_coded))
 }
--- src/routes/path_variables.rs
+++ src/routes/path_variables.rs
@@ -5,3 +5,6 @@ use axum::extract::Path;
 pub async fn path_variable(Path(id):Path<i32>)->String{
     id.to_string()
 }
+pub async fn hard_coded()->String{
+   "this is me".to_owned()
+}
