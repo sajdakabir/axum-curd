80b0603f81f979cc59f260503c42e252d9892320
sajdkabir <sajda.kbir@gmail.com>
2024-09-21 19:35:34 +0530

    geting jason data

--- src/routes/mirror_body_json.rs
+++ src/routes/mirror_body_json.rs
@@ -1,3 +1,20 @@
-pub async fn mirror_body_json (){
+use axum::Json;
+use serde::{Serialize, Deserialize};
 
+#[derive(Serialize, Deserialize, Debug)]
+pub struct  MirrorJson {
+    text: String
+}
+
+#[derive(Serialize)]
+pub struct  MirrorJsonResponce {
+    text: String,
+    extra: String
+}
+
+pub async fn mirror_body_json (Json(body):Json<MirrorJson>)-> Json<MirrorJsonResponce>{
+   Json(MirrorJsonResponce{
+        text: body.text ,
+        extra: "ohoo..".to_owned(),
+    })
 }
