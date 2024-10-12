1c62fc93eeec26fb21c86b785ca234a7ad0c3bb8
sajdkabir <sajda.kbir@gmail.com>
2024-10-12 12:32:31 +0530

    cors

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -5,15 +5,20 @@ mod path_variables;
 mod query_params;
 mod mirror_custion_hearder;
 
-use axum::{routing::{get, post}, Router};
+use axum::{http::Method, routing::{get, post}, Router};
 use hello_world::hello_world;
 use mirror_body_string::mirror_body_string;
 use mirror_body_json::mirror_body_json;
 use path_variables::{hard_coded, path_variable};
 use query_params::query_params;
 use mirror_custion_hearder:: mirror_custion_hearder;
+use tower_http::cors::{Any, CorsLayer};
 
 pub fn create_route()-> Router{
+    let cors= CorsLayer::new()
+    .allow_methods([Method::GET, Method::POST])
+    .allow_origin(Any);
+
     Router::new().route("/hello", get(hello_world))
                 .route("/mirror_body_string", post(mirror_body_string))
                 .route("/mirror_body_json", post(mirror_body_json))
@@ -21,4 +26,5 @@ pub fn create_route()-> Router{
                 .route("/path_variable/100", get(hard_coded))
                 .route("/queary", get(query_params))
                 .route("/header", get(mirror_custion_hearder))
+                .layer(cors)
 }
